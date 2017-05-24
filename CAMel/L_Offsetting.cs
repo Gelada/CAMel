cb80e603a7fb6b7588936f064c8d7ee770f1f2d1
Zac Slade <krakrjak@gmail.com>
2017-05-24 18:50:45 -0500

    Initial support for pocketing (WIP)
      GUI widgets are available, the solve method is a noop for now.

---
+++ CAMel/L_Offsetting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Grasshopper.Kernel.Data;
+using Rhino.Geometry;
+
+namespace CAMel.Types
+{
+
+    // Wrapper clases and library interfaces for leveraging the clipper
+    // library for doing offsetting
+    public class Offsetting
+    {
+       
+    }
+}
