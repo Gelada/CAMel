987a636c420852e599576dafe2d958b9f986361c
Gelada <eoharriss@gmail.com>
2018-04-08 19:38:29 -0500

    Fix default direction for index 3-axis for tool direction pointing out.

--- CAMel/C_Index3Axis.cs
+++ CAMel/C_Index3Axis.cs
@@ -27,7 +27,7 @@ namespace CAMel
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddCurveParameter("Curves", "C", "The curves for the tip of the tool to follow", GH_ParamAccess.list);
-            pManager.AddVectorParameter("Direction", "D", "Direction of the tool.", GH_ParamAccess.item, new Vector3d(0,0,1));
+            pManager.AddVectorParameter("Direction", "D", "Direction of the tool.", GH_ParamAccess.item, new Vector3d(0,0,-1));
             pManager.AddGenericParameter("Material/Tool", "MT", "The material to cut and the tool to do it", GH_ParamAccess.item);
             pManager.AddGenericParameter("MaterialForm", "MF","The shape of the material to cut", GH_ParamAccess.item);
         }
