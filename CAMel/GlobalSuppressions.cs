48d24c3c50ebd998da62232c0c88d3b3d1782242
Gelada <eoharriss@gmail.com>
2019-01-12 16:11:16 -0800

    Suppress naming messages.

---
+++ CAMel/GlobalSuppressions.cs
@@ -0,0 +1,11 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:CAMel.Types.MachineOperation.AddRange(System.Collections.Generic.IEnumerable{CAMel.Types.ToolPath})")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:CAMel.Types.ToolPath.AddRange(System.Collections.Generic.IEnumerable{Rhino.Geometry.Point3d})")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:CAMel.Types.ToolPath.AddRange(System.Collections.Generic.IEnumerable{CAMel.Types.ToolPoint})")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:CAMel.Types.ToolPath.Add(Rhino.Geometry.Point3d)")]
+
