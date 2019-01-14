4355b4b86eb957fa44ef4fa58d898c73d263796f
Gelada <eoharriss@gmail.com>
2019-01-13 21:09:00 -0800

    Finish making PreviewLength a const.

--- CAMel/P_ToolPoint.cs
+++ CAMel/P_ToolPoint.cs
@@ -159,10 +159,10 @@ namespace CAMel.Types
         }
 
         private const double previewLength = 0.2;
-        internal Line toolLine() => new Line(this.pt, this.pt + this.dir* this.previewLength);
+        internal Line toolLine() => new Line(this.pt, this.pt + this.dir* previewLength);
         public ToolPath getSinglePath() => new ToolPath() { this };
 
-        public BoundingBox getBoundingBox() => new BoundingBox(new List<Point3d> { this.pt, this.pt + this.dir * this.previewLength });
+        public BoundingBox getBoundingBox() => new BoundingBox(new List<Point3d> { this.pt, this.pt + this.dir * previewLength });
 
     }
 
