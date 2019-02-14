21065c9f83fc03165d55133480c1fc253e11c53c
Gelada <eoharriss@gmail.com>
2019-02-14 12:06:09 -0800

    Removes newline from additions exception.

--- CAMel/L_Exceptions.cs
+++ CAMel/L_Exceptions.cs
@@ -6,7 +6,7 @@ namespace CAMel
     {
         public static void matToolException() => throw new InvalidOperationException("Attempting to use ToolPath with no Material Tool information.");
         public static void matFormException() => throw new InvalidOperationException("Attempting to use ToolPath with no MaterialForm information.");
-        public static void additionsException() => throw new InvalidOperationException("Cannot write Code for toolpaths with unprocessed additions (such as step down or insert and retract moves.\n");
+        public static void additionsException() => throw new InvalidOperationException("Cannot write Code for toolpaths with unprocessed additions (such as step down or insert and retract moves.");
         public static void noToolException() => throw new InvalidOperationException("Tool not found, the list of Material Tools in your machine is either missing the named tool or simply empty.");
     }
 }
